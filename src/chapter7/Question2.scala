d9ddff46aa87892a13e50110a70956d6fc76678c
Denise Fernandez <dcbfernandez@gmail.com>
2014-02-09 18:45:04 +1100

    Chapter 7, question 2

---
+++ src/chapter7/Question2.scala
@@ -0,0 +1,13 @@
+package chapter7
+
+package horstmann {
+  package impatient {
+    package com {
+      class Question2 {
+        //Not sure what this question was actually asking for, but I've defined a package com which isn't at the top level!
+      }
+    }
+  }
+}
+
+
