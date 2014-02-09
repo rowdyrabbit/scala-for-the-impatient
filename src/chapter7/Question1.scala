d06a3eab1fc23c94ee11b520d2786136ac08b068
Denise Fernandez <dcbfernandez@gmail.com>
2014-02-09 18:19:59 +1100

    Adding chapter 7 question 1

---
+++ src/chapter7/Question1.scala
@@ -0,0 +1,28 @@
+package chapter7
+
+package com.horstmann {
+  object Utils {
+    def sayHi(msg: String) = {
+      "Hi! "+msg
+    }
+  }
+}
+
+package com.horstmann.impatient {
+  class TryToSayHi {
+
+    //The following won't compile because this package (com.horstmann.impatient) does not have access to the com.horstmann package
+    Utils.sayHi("I don't think I can say hi")
+  }
+}
+
+package com {
+  package horstmann {
+    package impatient {
+      class CanSayHi {
+          Utils.sayHi("I can say hi!")
+      }
+    }
+  }
+}
+
